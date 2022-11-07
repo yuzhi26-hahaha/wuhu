8bea4d65e56e8c68ecd29e4b822c1125a22dc9e0
yuzhi26-hahaha <72509140+yuzhi26-hahaha@users.noreply.github.com>
2022-11-07 09:43:25 +0800

    Add files via upload

---
+++ mian-ch.py
@@ -0,0 +1,57 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+today = datetime.now()
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
+
+
+def get_weather():
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get(url).json()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor(weather['temp'])
+
+
+def get_count():
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
+
+def get_graduate_left():
+    grad = datetime(2025, 6, 30)
+    toda = datetime.now()
+    return (grad - toda).days
+
+
+def get_words():
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
+
+def get_random_color():
+    return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"weather": {"value": wea}, "temperature": {"value": temperature}, "love_days": {"value": get_count()},
+        "graduate_days_left": {"value": get_graduate_left()}, "words": {"value": get_words(), "color": get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
